d9cca767732980ed09b6084b03f53a6078213cfe
Bulat Sitdikov <rdvad2@gmail.com>
2022-11-26 22:15:29 +0300

    feat: add models

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
